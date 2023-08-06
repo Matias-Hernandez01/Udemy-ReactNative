bb9f08ed6b8af8be4ea08f57b9c77d191b179f7b
Matias Hernandez <matias2k00@gmail.com>
2023-08-06 11:00:20 -0300

    flex

--- 02-MiPrimeraApp/App.tsx
+++ 02-MiPrimeraApp/App.tsx
@@ -3,13 +3,14 @@ import React from "react";
 // import { CounterScreen } from "./src/screens/CounterScreen";
 // import { BoxObjectModelScreen } from "./src/screens/BoxObjectModelScreen";
 // import { DimensionesScreen } from "./src/screens/DimensionesScreen";
+// import { PositionScreen } from "./src/screens/PositionScreen";
 import { SafeAreaView } from "react-native";
-import { PositionScreen } from "./src/screens/PositionScreen";
+import { FlexScreen } from "./src/screens/FlexScreen";
 
 export const App = () => {
   return( 
     <SafeAreaView style={{flex:1}}>
-      <PositionScreen />
+      <FlexScreen />
     </SafeAreaView>
   )
 }
