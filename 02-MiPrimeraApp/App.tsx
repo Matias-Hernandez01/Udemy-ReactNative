5fde7352debb878f680dd0cc8deaa668ca17f231
Matias Hernandez <matias2k00@gmail.com>
2023-08-05 21:39:49 -0300

    object model box

--- 02-MiPrimeraApp/App.tsx
+++ 02-MiPrimeraApp/App.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 // import HolaMundoScreen from "./src/screens/HolaMundoScreen";
-import { CounterScreen } from "./src/screens/CounterScreen";
-import { BoxObjectModelScreen } from "./src/screens/BoxObjectModelScreen";
+// import { CounterScreen } from "./src/screens/CounterScreen";
+// import { BoxObjectModelScreen } from "./src/screens/BoxObjectModelScreen";
 import { SafeAreaView } from "react-native";
+import { DimensionesScreen } from "./src/screens/DimensionesScreen";
 
 
 export const App = () => {
   return( 
-    <SafeAreaView>
-      <BoxObjectModelScreen />
+    <SafeAreaView style={{flex:1}} >
+      <DimensionesScreen />
     </SafeAreaView>
   )
 }
--- 02-MiPrimeraApp/src/screens/BoxObjectModelScreen.tsx
+++ 02-MiPrimeraApp/src/screens/BoxObjectModelScreen.tsx
@@ -1,10 +1,22 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export const BoxObjectModelScreen = () => {
   return (
-    <View>
-      <Text>Box Object Model</Text>
+    <View style={styles.container} >
+      <Text style={styles.title} >Box Object Model</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container:{
+      backgroundColor:"red",
+      flex:1,
+    },
+    title:{
+      borderWidth: 2,
+      marginHorizontal:10,
+      
+    }
+});
