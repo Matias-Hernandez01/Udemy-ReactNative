9fd27c3dcdff5c62a1c1b09f8685260183b37ae5
Matias Hernandez <matias2k00@gmail.com>
2023-09-02 20:27:41 -0300

    obteniendo colores

--- PeliculasApp/src/screens/HomeScreen.tsx
+++ PeliculasApp/src/screens/HomeScreen.tsx
@@ -1,36 +1,35 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import {View, ActivityIndicator, Dimensions, ScrollView} from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import Carousel from 'react-native-snap-carousel';
-import ImageColors from 'react-native-image-colors';
 
 
 import { UseMovies } from "../hooks/useMovies";
 import { MovieCard } from "../components/MovieCard";
 import { FlatListComponents } from "../components/FlatListComponents";
 import { GradientBackground } from "../components/GradientBackground";
-
+import {getImageColors} from "../helpers/getColores";
 
 
 const {width: windowWidth} = Dimensions.get("window");
 
 export const HomeScreen = () => {
 
+
   const {nowPlaying,popular,topRated, upcoming, isLoading} = UseMovies();
   const {top} = useSafeAreaInsets()
 
-  const getPosterColors = async (index: number)=>{
-    
+  const getPosterColors = async (index: number) => {
+
     const movie = nowPlaying[index];
     const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-    console.log(nowPlaying)
-    //   // const colors = await ImageColors.getColors(uri, {});    
-    //   // console.log(colors)
-    // }
 
-    console.log(movie)
+    const [primary, secondary] = await getImageColors(uri)
 
+    console.log(primary)
   }
+  
+
 
   if(isLoading){
     return <View style={{flex:1, justifyContent:"center", alignItems:"center"}} >
