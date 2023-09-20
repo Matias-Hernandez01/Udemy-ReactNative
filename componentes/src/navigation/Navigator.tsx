c85af6202b39a2b34005d2b7a944456784a8d46f
Matias Hernandez <matias2k00@gmail.com>
2023-09-20 19:12:41 -0300

    navigator

---
+++ componentes/src/navigation/Navigator.tsx
@@ -0,0 +1,16 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import { Home } from '../screens/Home';
+
+const Stack = createStackNavigator();
+
+export const Navigator = () =>  {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false
+      }}
+    >
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+  );
+}
