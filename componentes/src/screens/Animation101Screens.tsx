30cc756f6505d6c5fcfc5cab0eb68250c0afd2e7
Matias Hernandez <matias2k00@gmail.com>
2023-09-21 00:16:34 -0300

    optimizando codigo utilizando custom hooks

--- componentes/src/screens/Animation101Screens.tsx
+++ componentes/src/screens/Animation101Screens.tsx
@@ -1,43 +1,13 @@
 import React, {useRef} from "react";
-
 import { View, StyleSheet,Animated, Button, Easing } from 'react-native';
+
+import { useAnimation } from "../hooks/useAnimation";
 
 
 export const Animation101Screens = () => {
 
-  const opacity = useRef(new Animated.Value(0)).current;
-  const top = useRef(new Animated.Value(-100)).current;
+  const {opacity, position, fadeIn, fadeOut, startPosition} = useAnimation();
 
-  const fadeIn = ()=>{
-    Animated.timing(opacity, {
-      toValue: 1,
-      duration: 300,
-      useNativeDriver: true,
-    }).start();
-
-    Animated.timing(top, {
-      toValue:0,
-      duration:800,
-      useNativeDriver:true,
-      easing: Easing.bounce
-    }).start();
-
-  }
-
-  const fadeOut = () =>{
-    Animated.timing(opacity,
-      {
-        toValue:0,
-        duration: 700,
-        useNativeDriver: true,
-      }).start()
-    
-      Animated.timing(top, {
-        toValue:-150,
-        duration: 700,
-        useNativeDriver: true
-      }).start()
-  }
 
   return (
     <View style={styles.container} >
@@ -45,7 +15,7 @@ export const Animation101Screens = () => {
         {...styles.purpleBox,
           opacity: opacity,
           transform: [{
-            translateY: top,
+            translateY: position,
           }],
           
         }} />
@@ -56,7 +26,7 @@ export const Animation101Screens = () => {
         }} >
           <Button 
             title="FadeIn"
-            onPress={fadeIn}
+            onPress={() => {fadeIn(), startPosition(-150)}}
           />
       </View>
         <View>
