4f7cd954550a7224ec15af82a0dacce64e04a2a4
Matias Hernandez <matias2k00@gmail.com>
2023-09-20 23:57:59 -0300

    animation screen 2

---
+++ componentes/src/screens/Animation102Screens.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { View, StyleSheet } from 'react-native';
+
+
+export const Animation102Screens = () => {
+  return (
+    <View style={{flex:1}} >
+      <View style={styles.purpleBox} />
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  purpleBox:{
+    backgroundColor: "red",
+    width: 150,
+    height:150,
+  }
+});
