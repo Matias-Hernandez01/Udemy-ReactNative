e5a1331c597f7691eb04a2bc953fe08926ef156a
Matias Hernandez <matias2k00@gmail.com>
2023-09-21 23:16:14 -0300

    date

---
+++ componentes/src/data/menuItems.tsx
@@ -0,0 +1,29 @@
+
+
+export const menuItems : MenuItem[] = [
+  {
+    name: "Animation 101",
+    icon: "cube-outline",
+    component: "Animation101Screens"
+  },
+  {
+    name: "Animation 102",
+    icon: "cellular-outline",
+    component: "Animation102Screens"
+  },
+  {
+    name: "Switches",
+    icon: "toggle-outline",
+    component: "SwitchScreens"
+  },
+  {
+    name: "Alerts",
+    icon: "alert-circle-outline",
+    component: "AlertScreens"
+  },
+  {
+    name: "TextInput",
+    icon: "document-text-outline",
+    component: "TextInputScreens"
+  }
+]
