b969845e5d63c9ddc0b862dea18af45e1c03041a
Matias Hernandez <matias2k00@gmail.com>
2023-09-21 23:16:51 -0300

    stack navigator

--- componentes/src/navigation/Navigator.tsx
+++ componentes/src/navigation/Navigator.tsx
@@ -2,6 +2,9 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { Home } from '../screens/Home';
 import { Animation101Screens } from '../screens/Animation101Screens';
 import { Animation102Screens } from '../screens/Animation102Screens';
+import { SwitchScreens } from '../screens/SwitchScreens';
+import { AlertScreens } from '../screens/AlertScreens';
+import { TextInputScreens } from '../screens/TextInputScreens';
 
 const Stack = createStackNavigator();
 
@@ -15,6 +18,10 @@ export const Navigator = () =>  {
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="Animation101Screens" component={Animation101Screens} />
       <Stack.Screen name="Animation102Screens" component={Animation102Screens} />
+      <Stack.Screen name="SwitchScreens" component={SwitchScreens} />
+      <Stack.Screen name="AlertScreens" component={AlertScreens} />
+      <Stack.Screen name="TextInputScreens" component={TextInputScreens} />
+
     </Stack.Navigator>
   );
 }
