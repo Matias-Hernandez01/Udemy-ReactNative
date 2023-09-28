666b9811075026e0fda5f56a91f833484c2d8e2f
Matias Hernandez <matias2k00@gmail.com>
2023-09-27 22:13:01 -0300

    ejercicio terminadop

--- componentes/src/screens/TextInputScreens.tsx
+++ componentes/src/screens/TextInputScreens.tsx
@@ -1,49 +1,59 @@
 import React, {useState} from "react";
 
-import { View, TextInput, StyleSheet } from 'react-native';
+import { View, TextInput, StyleSheet, KeyboardAvoidingView, Platform, Text } from 'react-native';
 import { HeaderTitle } from '../components/HeaderTitle';
 import { styles } from '../theme/appTheme';
+import { ScrollView } from "react-native-gesture-handler";
+import { useForm } from "../hooks/useForm";
+import { CumstomSwitch } from "../components/CumstomSwitch";
 
 
 export const TextInputScreens = () => {
 
-  const [form, setForm] = useState({
+  const {onChange, form, isSuscribed} = useForm({
     name:"",
     email:"",
-    phone:""
-  })
-
-  const onChangeForm = (value: string, name: string)=>{
-    setForm({...form, [name]: value})
-  }
-
+    phone:"",
+    isSuscribed:false,
+  });
 
   return (
+    <KeyboardAvoidingView
+    behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
+    <ScrollView>
+      
     <View style={styles.globalMargin} >
       <HeaderTitle title="TextInputs"/>
+      <HeaderTitle title={JSON.stringify(form, null, 3)}/>
+      <Text>Suscribirme</Text>
+      <CumstomSwitch 
+        isOn={isSuscribed}
+        onChange={(value)=> onChange( isSuscribed, "isSuscribed") }
+      />
       <TextInput 
         style={style.input}
         placeholder="Ingrese su nombre"
-        value={form.name}
-        onChangeText={(value)=> onChangeForm(value, "name")}
+        value="name"
+        onChangeText={(value)=> onChange(value, "name")}
         />
       <TextInput 
         style={style.input}
         placeholder="Ingrese su email"
-        onChangeText={(value)=> onChangeForm(value, "email")}
+        onChangeText={(value)=> onChange(value, "email")}
         
         />
-      <TextInput 
+      <HeaderTitle title={JSON.stringify(form, null, 3)}/>
+      <TextInput  
         style={style.input}
         placeholder="Ingrese su telefono"
-        onChangeText={(value)=> onChangeForm(value, "phone")}
+        onChangeText={(value)=> onChange(value, "phone")}
         keyboardType="phone-pad"
-      />
-
-    <HeaderTitle title={JSON.stringify(form, null, 3)}/>
-
-
-    </View>
+        />
+  
+       </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   )
 }
 
