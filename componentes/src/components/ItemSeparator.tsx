3d96d868272c331bd8a970860b21ac9e30231dc0
Matias Hernandez <matias2k00@gmail.com>
2023-09-28 13:57:57 -0300

    component item separator

---
+++ componentes/src/components/ItemSeparator.tsx
@@ -0,0 +1,15 @@
+import { View } from 'react-native';
+
+
+export const ItemSeparator = () => {
+  return (
+    <View 
+    style={{
+      backgroundColor:"#5856D6",
+       borderBottomWidth: 1,
+       opacity: 0.4,
+       marginVertical:8
+    }}
+  />
+  )
+}
