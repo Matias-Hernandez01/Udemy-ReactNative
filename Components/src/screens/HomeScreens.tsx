81bb353c6620e3025ec9fe1dce7e590cadda6fbf
Matias Hernandez <matias2k00@gmail.com>
2023-10-03 20:08:06 -0300

    home screens

---
+++ Components/src/screens/HomeScreens.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export const HomeScreens = () => {
+  return (
+    <View>
+      <Text>HomeScreens</Text>
+    </View>
+  )
+}
