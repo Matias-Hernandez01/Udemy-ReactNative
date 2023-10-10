beb101d224118a12b47404babe0dc18f0d6f9a3e
Matias Hernandez <matias2k00@gmail.com>
2023-10-10 13:21:51 -0300

    add inifiniteScroll

--- componentes/src/navigation/Navigator.tsx
+++ componentes/src/navigation/Navigator.tsx
@@ -1,32 +1,45 @@
-import { createStackNavigator } from '@react-navigation/stack';
-import { Home } from '../screens/Home';
-import { Animation101Screens } from '../screens/Animation101Screens';
-import { Animation102Screens } from '../screens/Animation102Screens';
-import { SwitchScreens } from '../screens/SwitchScreens';
-import { AlertScreens } from '../screens/AlertScreens';
-import { TextInputScreens } from '../screens/TextInputScreens';
-import { PullToRefreshScreens } from '../screens/PullToRefreshScreens';
-import { SectionListScreens } from '../screens/SectionListScreens';
-import { ModalScreen } from '../screens/ModalScreen';
+import {createStackNavigator} from '@react-navigation/stack';
+import {Home} from '../screens/Home';
+import {Animation101Screens} from '../screens/Animation101Screens';
+import {Animation102Screens} from '../screens/Animation102Screens';
+import {SwitchScreens} from '../screens/SwitchScreens';
+import {AlertScreens} from '../screens/AlertScreens';
+import {TextInputScreens} from '../screens/TextInputScreens';
+import {PullToRefreshScreens} from '../screens/PullToRefreshScreens';
+import {SectionListScreens} from '../screens/SectionListScreens';
+import {ModalScreen} from '../screens/ModalScreen';
+import {InfinitiScrollScreens} from '../screens/InfinitiScrollScreens';
 
 const Stack = createStackNavigator();
 
-export const Navigator = () =>  {
+export const Navigator = () => {
   return (
     <Stack.Navigator
       screenOptions={{
-        headerShown: false
-      }}
-    >
+        headerShown: false,
+      }}>
       <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Animation101Screens" component={Animation101Screens} />
-      <Stack.Screen name="Animation102Screens" component={Animation102Screens} />
+      <Stack.Screen
+        name="Animation101Screens"
+        component={Animation101Screens}
+      />
+      <Stack.Screen
+        name="Animation102Screens"
+        component={Animation102Screens}
+      />
       <Stack.Screen name="SwitchScreens" component={SwitchScreens} />
       <Stack.Screen name="AlertScreens" component={AlertScreens} />
       <Stack.Screen name="TextInputScreens" component={TextInputScreens} />
-      <Stack.Screen name="PullToRefreshScreens" component={PullToRefreshScreens} />
+      <Stack.Screen
+        name="PullToRefreshScreens"
+        component={PullToRefreshScreens}
+      />
       <Stack.Screen name="SectionListScreens" component={SectionListScreens} />
       <Stack.Screen name="ModalScreens" component={ModalScreen} />
+      <Stack.Screen
+        name="InfinitiScrollScreens"
+        component={InfinitiScrollScreens}
+      />
     </Stack.Navigator>
   );
-}
+};
