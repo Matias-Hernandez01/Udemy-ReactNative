cd014763a5864d61855b6f35e8b4654387e6e6ec
Matias Hernandez <matias2k00@gmail.com>
2023-10-10 13:22:15 -0300

    flatList, infiniteScroll

---
+++ componentes/src/screens/InfinitiScrollScreens.tsx
@@ -0,0 +1,50 @@
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, FlatList} from 'react-native';
+
+import {HeaderTitle} from '../components/HeaderTitle';
+import {styles} from '../theme/appTheme';
+
+export const InfinitiScrollScreens = () => {
+  const [numbers, setNumbers] = useState([0, 1, 2, 3, 4, 5]);
+
+  const renderItem = (item: number) => {
+    return <Text style={styless.textItem}>{item}</Text>;
+  };
+
+  const loadMore = () => {
+    const newArray: number[] = [];
+    for (let i = 0; i < 5; i++) {
+      newArray[i] = numbers.length + i;
+    }
+
+    setNumbers([...numbers, ...newArray]);
+  };
+
+  return (
+    <View style={styless.mainContainer}>
+      <FlatList
+        data={numbers}
+        keyExtractor={item => item.toString()}
+        renderItem={({item}) => renderItem(item)}
+        ListHeaderComponent={<HeaderTitle title="IfiniteScroll" />}
+        onEndReached={loadMore}
+        onEndReachedThreshold={0.5}
+      />
+    </View>
+  );
+};
+
+const styless = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    backgroundColor: '#b0c2f2',
+  },
+  textItem: {
+    backgroundColor: '#84b6f4',
+    height: 100,
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+    paddingLeft: 10,
+  },
+});
