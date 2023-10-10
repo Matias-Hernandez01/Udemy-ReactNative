eeca93fdc5a1393004cf5ede1b68259fde340c43
Matias Hernandez <matias2k00@gmail.com>
2023-10-10 13:21:33 -0300

    add flat list

--- componentes/src/data/menuItems.tsx
+++ componentes/src/data/menuItems.tsx
@@ -1,44 +1,47 @@
-
-
-export const menuItems : MenuItem[] = [
+export const menuItems: MenuItem[] = [
   {
-    name: "Animation 101",
-    icon: "cube-outline",
-    component: "Animation101Screens"
+    name: 'Animation 101',
+    icon: 'cube-outline',
+    component: 'Animation101Screens',
   },
   {
-    name: "Animation 102",
-    icon: "cellular-outline",
-    component: "Animation102Screens"
+    name: 'Animation 102',
+    icon: 'cellular-outline',
+    component: 'Animation102Screens',
   },
   {
-    name: "Switches",
-    icon: "toggle-outline",
-    component: "SwitchScreens"
+    name: 'Switches',
+    icon: 'toggle-outline',
+    component: 'SwitchScreens',
   },
   {
-    name: "Alerts",
-    icon: "alert-circle-outline",
-    component: "AlertScreens"
+    name: 'Alerts',
+    icon: 'alert-circle-outline',
+    component: 'AlertScreens',
   },
   {
-    name: "TextInput",
-    icon: "document-text-outline",
-    component: "TextInputScreens"
+    name: 'TextInput',
+    icon: 'document-text-outline',
+    component: 'TextInputScreens',
   },
   {
-    name:"Refresh",
-    icon:"refresh-outline",
-    component:"PullToRefreshScreens"
+    name: 'Refresh',
+    icon: 'refresh-outline',
+    component: 'PullToRefreshScreens',
   },
   {
-    name:"Section List",
-    icon:"list-outline",
-    component:"SectionListScreens"
+    name: 'Section List',
+    icon: 'list-outline',
+    component: 'SectionListScreens',
   },
   {
-    name:"Modal Screen",
-    icon:"copy-outline",
-    component:"ModalScreens"
-  }
-]
+    name: 'Modal Screen',
+    icon: 'copy-outline',
+    component: 'ModalScreens',
+  },
+  {
+    name: 'InfiniteScrol',
+    icon: 'download-outline',
+    component: 'InfinitiScrollScreens',
+  },
+];
