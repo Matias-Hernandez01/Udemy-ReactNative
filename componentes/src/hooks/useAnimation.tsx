d5e25a08b4de3da1789a26597ba440088442a1b6
Matias Hernandez <matias2k00@gmail.com>
2023-10-20 19:53:08 -0300

    hook useAnimation modificadio

--- componentes/src/hooks/useAnimation.tsx
+++ componentes/src/hooks/useAnimation.tsx
@@ -1,47 +1,41 @@
-import React,{useRef} from 'react';
-import { View, Animated, Easing } from 'react-native';
-
+import React, {useRef} from 'react';
+import {View, Animated, Easing} from 'react-native';
 
 export const useAnimation = () => {
-  
   const opacity = useRef(new Animated.Value(0)).current;
   const position = useRef(new Animated.Value(0)).current;
 
-  const fadeIn = ()=>{
+  const fadeIn = (duration: number = 300) => {
     Animated.timing(opacity, {
       toValue: 1,
-      duration: 300,
+      duration,
       useNativeDriver: true,
     }).start();
-  }
+  };
 
-  const fadeOut = () =>{
-    Animated.timing(opacity,
-      {
-        toValue:0,
-        duration: 400,
-        useNativeDriver: true,
-      }).start()
-    
-      Animated.timing(position, {
-        toValue:-150,
-        duration: 400,
-        useNativeDriver: true
-      }).start()
-  }
-
-  const startPosition = (initPosition: number) =>{
-    
-    position.setValue(initPosition)
-    Animated.timing(position, {
-      toValue:0,  
+  const fadeOut = () => {
+    Animated.timing(opacity, {
+      toValue: 0,
       duration: 400,
       useNativeDriver: true,
-      easing:Easing.bounce,
-    }).start()
+    }).start();
 
-  }
+    Animated.timing(position, {
+      toValue: -150,
+      duration: 400,
+      useNativeDriver: true,
+    }).start();
+  };
 
-  
-  return {opacity, position, fadeIn, fadeOut,startPosition}
-}
+  const startPosition = (initPosition: number) => {
+    position.setValue(initPosition);
+    Animated.timing(position, {
+      toValue: 0,
+      duration: 400,
+      useNativeDriver: true,
+      easing: Easing.bounce,
+    }).start();
+  };
+
+  return {opacity, position, fadeIn, fadeOut, startPosition};
+};
